38f3fe1efc6a8fc384dc6ec537dee877aeb7a002
Tyler Burnham <tylerburnham42@gmail.com>
2016-09-29 17:24:45 -0400

    Added GPS Support, Bearing issue present

--- improved_antenna.py
+++ improved_antenna.py
@@ -60,10 +60,10 @@ def moveToCenterPos():              #Send servos to their center pos (should be 
     else:
         print "Error: Settings set to no Servo Connection"
 
-
 def moveToTarget(bearing,elevation):        #moves servos based on a bearing and elevation angle
         
-        global centerBear,antennaBear,antennaEle
+        centerBear = 0
+
         temp = 0
         if((bearing>180) and (centerBear == 0)):
                 centerBear = 360
@@ -94,8 +94,7 @@ def moveToTarget(bearing,elevation):        #moves servos based on a bearing and
                 centerBear = temp
         #if servoAttached:
         #    s.close()
-        antennaBear = bearing
-        antennaEle = elevation
+
 
 
 
@@ -130,8 +129,51 @@ def movePanServo(position):
 #            s.write(movePan)
             print "\t\tMove Pan: ", float(position)
             s.close()
+        
+def pointToTarget(pointtoLon, pointtoLat, pointtoAlt):
+    groundLat = 32.827227
+    groundLon = -83.648384
+    groundAlt = 100
+    
+    deltaAlt = float(pointtoAlt) - groundAlt
+    distanceToTarget = 0.87*haversine( groundLat,  groundLon, pointtoLat, pointtoLon)
+    bearingToTarget = bearing(groundLat,  groundLon, pointtoLat, pointtoLon)   
+    elevationAngle = math.degrees(math.atan2(deltaAlt,distanceToTarget))
+    print(distanceToTarget,bearingToTarget,elevationAngle)
+    moveToTarget(bearingToTarget,elevationAngle)
 
-         
+
+# great circle bearing, see: http://www.movable-type.co.uk/scripts/latlong.html 
+def bearing(trackerLat, trackerLon, remoteLat, remoteLon):
+        dLat = math.radians(remoteLat-trackerLat)       # delta latitude in radians
+        dLon = math.radians(remoteLon-trackerLon)       # delta longitude in radians
+        
+        y = math.sin(dLon)*math.cos(math.radians(remoteLat))
+        x = math.cos(math.radians(trackerLat))*math.sin(math.radians(remoteLat))-math.sin(math.radians(trackerLat))*math.cos(math.radians(remoteLat))*math.cos(dLat)
+        tempBearing = math.degrees(math.atan2(y,x))     # returns the bearing from true north
+        if (tempBearing < 0):
+                tempBearing = tempBearing + 360
+        return tempBearing
+
+    ##############################
+    ## Returns Data in Nautical ##
+    ##############################
+
+# haversine formula, see: http://www.movable-type.co.uk/scripts/latlong.html    
+def haversine(trackerLat, trackerLon, remoteLat, remoteLon):
+        R = 6371        # radius of earth in Km
+
+        dLat = math.radians(remoteLat-trackerLat)       # delta latitude in radians
+        dLon = math.radians(remoteLon-trackerLon)       # delta longitude in radians
+        ####################################
+        a = math.sin(dLat/2)*math.sin(dLat/2)+math.cos(math.radians(trackerLat))*math.cos(math.radians(remoteLat))*math.sin(dLon/2)*math.sin(dLon/2)
+        #############################
+        c = 2*math.atan2(math.sqrt(a),math.sqrt(1-a))
+        
+        d = R*c
+        
+        return d*3280.839895 # multiply distance in Km by 3280 for feet
+
          
          
 if __name__ == "__main__":
@@ -141,14 +183,20 @@ if __name__ == "__main__":
     
 #    val = 10    
     
-    moveToCenterPos()    
+    #moveToCenterPos()    
+    #32.827227, -83.648384
+    pointToTarget(32.827227, -83.648384, 0)
+    print("North")
+    time.sleep(2)
 
-    val1 = 10    
+    pointToTarget(32.827607, -83.648578, 0)
+    print("North")
+    time.sleep(2)
     
-    while(val1 != 0):
-        val1,val2 = [float(x) for x in input()]
-        moveToTarget(val1, val2)
-        #time.sleep(2)
-        #val = input("Val?")        
-        #movePanServo(val)  
-        #time.sleep(2)
+    pointToTarget(32.818444, -83.648427, 0)
+    print("South")
+    time.sleep(2)    
+    
+    pointToTarget(0, 0, 0)
+    print("South")
+    time.sleep(2)
